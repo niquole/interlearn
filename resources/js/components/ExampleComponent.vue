2da5cdef2885387e659db1d43a326ca266da4bc5
niquole <nicole.antonopoulou@gmail.com>
2020-11-14 12:14:22 +0200

    element and api works

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <ul id="example-1">
+  <li v-for="(user,i) in users" :key="i" >
+    {{ user.name }}
+  </li>
+</ul>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        users: []
+      }
+    },
+    mounted() {
+        axios.get('/api/users').then((response) => {
+            this.users = response.data
+        })
+    }
+  }
+</script>
