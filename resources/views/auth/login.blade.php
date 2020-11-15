c22e970fd6d76a4229cbef9c2b27e036fad25f58
niquole <nicole.antonopoulou@gmail.com>
2020-11-15 14:20:12 +0200

    login page done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,6 @@
     <div class="row justify-content-center ">
         <div class="col-md-5 ">
             <div class="card">
-                <h1 class="card-header  ">{{ __('Login') }}</h1>
                 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
@@ -13,9 +12,10 @@
                         
                         <div class="form-group row">
                             <!-- <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label> -->
+                            <h1 class="card-header  center">{{ __('Login') }}</h1>
                             
                             <div class="col-md-12">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" placeholder="Email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                                 
                                 @error('email')
                                 <span class="invalid-feedback" role="alert">
@@ -29,7 +29,7 @@
                             <!-- <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label> -->
                             
                             <div class="col-md-12">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input id="password" placeholder="Password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
                                 
                                 @error('password')
                                 <span class="invalid-feedback" role="alert">
@@ -39,8 +39,8 @@
                             </div>
                         </div>
                         
-                        <div class="form-group row ">
-                            <div class="col-md-6">
+                        <div class="form-group row align-right">
+                            <div class="col-md-12">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
                                     
@@ -51,36 +51,35 @@
                             </div>
                         </div>
                         
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                        <div class="form-group">
+                            <div class="align-center">
+                                <el-button type="submit" round class="login-button">
                                     {{ __('Login') }}
-                                </button>
+                                </el-button>
                                 
                                 @if (Route::has('password.request'))
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
+                                <div class="center align-center">
+                                    <a class="btn btn-link mt-2 forgot" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                </div>
                                 @endif
                             </div>
                         </div>
                     </form>
                 </div>
+                <div class="center mt-5">
+                    <span>New to Interlearn? <br></span>
+                    <a class="nav-link align-center join" href="{{ route('register') }}">{{ __('Join Now') }}</a>
+                </div>
             </div>
-            <span>Not a member? <br></span>
-                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+
         </div>
     </div>
 </div>
 @endsection
 
 
-<style>
-.form-control {
-    background-color: transparent !important;
-    /* border-radius: 15px !important; */
-    border: none !important;
-    border-bottom: 2px solid #fff !important;
-}
-    
+<style scoped>
+
 </style>
